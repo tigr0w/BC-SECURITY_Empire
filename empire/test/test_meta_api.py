8feda57f89ff4856cb73c9c4a118fd7de5d90e1d
Vince Rose <vrose04@gmail.com>
2022-07-29 20:35:28 -0700

    bump starkiller to first sponsor beta build

--- empire/test/test_meta_api.py
+++ empire/test/test_meta_api.py
@@ -1,7 +1,8 @@
 def test_version(client, admin_auth_header):
-    response = client.get("/api/v2/meta/version", headers=admin_auth_header)
+    import empire.server.common.empire
 
+    response = client.get("/api/v2/meta/version", headers=admin_auth_header)
     assert response.status_code == 200
     assert (
-        response.json()["version"] == "5.0.0-alpha1"
-    )  # todo move version to a constant that can be imported by the tests and the server
+        response.json()["version"] == empire.server.common.empire.VERSION.split(" ")[0]
+    )
